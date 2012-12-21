e5785ce743c037407e2ef7395e74c6e510e7f65d
John Wiegley <johnw@fpcomplete.com>
2012-12-21 06:19:10 -0600

    Parser is mostly working now

--- ledger-parse/Ledger/Parser/Text.hs
+++ ledger-parse/Ledger/Parser/Text.hs
@@ -1,12 +1,21 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module Ledger.Parser.Text ( parseJournalFile ) where
+module Ledger.Parser.Text
+       ( parseJournalFile
+       , RawJournal(..)
+       , RawEntity(..)
+       , RawEntityInSitu(..)
+       , RawPosting(..)
+       , RawTransaction(..)
+       , RawAutoTxn(..)
+       , RawPeriodTxn(..)
+       ) where
 
 import Control.Applicative
-import Data.ByteString as B hiding (pack, unpack, singleton)
+import Data.ByteString as B hiding (pack, unpack, singleton, zipWith)
 import Data.Char (isSpace)
 import Data.Maybe
-import Data.Text as T
+import Data.Text as T hiding (zipWith)
 import Data.Text.Encoding as E
 import Debug.Trace (trace)
 import Filesystem.Path.CurrentOS
@@ -31,43 +40,44 @@ data RawEntity = Whitespace Text
                | Directive { directiveChar :: Maybe Char
                            , directiveName :: Text
                            , directiveArg  :: Maybe Text }
-               | RawTxnEntity RawTxn
+               | RawTransactionEntity RawTransaction
                | RawAutoTxnEntity RawAutoTxn
                | RawPeriodTxnEntity RawPeriodTxn
                | EndOfFile
                deriving (Show, Eq)
 
-data RawEntityInSitu = RawEntityInSite { rawEntityIndex   :: Int
-                                       , rawEntityAbsPath :: Text
-                                       , rawEntityLineNo  :: Int
-                                       , rawEntity        :: RawEntity }
-                     deriving (Show, Eq)
+data RawEntityInSitu = RawEntityInSitu { rawEntityIndex    :: Int
+                                       , rawEntityStartPos :: Rendering
+                                       , rawEntity         :: RawEntity
+                                       , rawEntityEndPos   :: Rendering }
 
-data RawPost = RawPost { rawPostState           :: Maybe Char
-                       , rawPostAccount         :: Text
-                       , rawPostAmount          :: Maybe Text
-                       , rawPostCost            :: Maybe (Text,Text)
-                       , rawPostAssignedBalance :: Maybe Text
-                       , rawPostNote            :: Maybe Text }
-             deriving (Show, Eq)
+instance Show RawEntityInSitu where
+  show x = show (rawEntity x) ++ "\n"
 
-data RawTxn = RawTxn { rawTxnDate    :: Text
-                     , rawTxnDateAux :: Maybe Text
-                     , rawTxnState   :: Maybe Char
-                     , rawTxnCode    :: Maybe Text
-                     , rawTxnDesc    :: Text
-                     , rawTxnNote    :: Maybe Text
-                     , rawTxnPosts   :: [RawPost] }
-            deriving (Show, Eq)
+data RawPosting = RawPosting { rawPostState           :: Maybe Char
+                             , rawPostAccount         :: Text
+                             , rawPostAmount          :: Maybe Text
+                             , rawPostCost            :: Maybe (Text,Text)
+                             , rawPostAssignedBalance :: Maybe Text
+                             , rawPostNote            :: Maybe Text }
+                | RawPostingNote Text
+                deriving (Show, Eq)
+
+data RawTransaction = RawTransaction { rawTxnDate    :: Text
+                                     , rawTxnDateAux :: Maybe Text
+                                     , rawTxnState   :: Maybe Char
+                                     , rawTxnCode    :: Maybe Text
+                                     , rawTxnDesc    :: Text
+                                     , rawTxnNote    :: Maybe Text
+                                     , rawTxnPosts   :: [RawPosting] }
+                    deriving (Show, Eq)
 
 data RawAutoTxn = RawAutoTxn { rawATxnQuery :: Text
-                             , rawATxnNote  :: Maybe Text
-                             , rawATxnPosts :: [RawPost] }
+                             , rawATxnPosts :: [RawPosting] }
                 deriving (Show, Eq)
 
 data RawPeriodTxn = RawPeriodTxn { rawPTxnPeriod :: Text
-                                 , rawPTxnNote   :: Maybe Text
-                                 , rawPTxnPosts  :: [RawPost] }
+                                 , rawPTxnPosts  :: [RawPosting] }
                   deriving (Show, Eq)
 
 txnDateParser :: TokenParsing m => m Text
@@ -103,18 +113,28 @@ untilLongSepOrEOLOr p = pack <$> ((:) <$> noneOf "\r\n"
 tokenP :: TokenParsing m => m p -> m p
 tokenP p = p <* skipMany spaceChars
 
-postingParser :: TokenParsing m => m RawPost
+postingParser :: TokenParsing m => m RawPosting
 postingParser =
-  RawPost <$> (some spaceChars *>
-               optional (tokenP (char '*' <|> char '!')))
-          <*> tokenP untilLongSepOrEOL
-          <*> optional (tokenP (untilLongSepOrEOLOr (some (oneOf "@="))))
-          <*> optional
-              ((,) <$> (T.filter (not . isSpace) . pack <$> some (oneOf "@= "))
-                   <*> tokenP (untilLongSepOrEOLOr (char '=')))
-          <*> optional (char '=' *> untilLongSepOrEOL)
-          <*> (optional (longSep *> noteParser) <* endOfLine)
-          <?> "posting"
+  (RawPosting
+  <$> (some spaceChars *>
+       optional (tokenP (char '*' <|> char '!')))
+  <*> tokenP untilLongSepOrEOL
+  <*> optional (tokenP (untilLongSepOrEOLOr (some (oneOf "@="))))
+  <*> optional
+      ((,) <$> (T.filter (not . isSpace) . pack <$>
+                some (oneOf "@= "))
+           <*> tokenP (untilLongSepOrEOLOr (char '=')))
+  <*> optional (char '=' *> untilLongSepOrEOL)
+  <*> (optional (longSep *> noteParser) <* endOfLine)
+  <?> "posting")
+
+  <|>
+
+  (RawPostingNote
+  <$> (T.concat <$>
+       some (T.append <$> (some spaceChars *> noteParser)
+                      <*> endOfLineText))
+  <?> "posting note")
 
 spaceChars :: CharParsing m => m ()
 spaceChars = oneOf " \t" *> pure ()
@@ -123,8 +143,8 @@ anySpaceChars :: CharParsing m => m ()
 anySpaceChars = oneOf " \t\r\n" *> pure ()
 
 regularTxnParser :: TokenParsing m => m RawEntity
-regularTxnParser = RawTxnEntity <$> go
-  where go = RawTxn
+regularTxnParser = RawTransactionEntity <$> go
+  where go = RawTransaction
              <$> txnDateParser
              <*> optional (char '=' *> txnDateParser)
              <*> (many spaceChars *>
@@ -136,10 +156,20 @@ regularTxnParser = RawTxnEntity <$> go
              <?> "regular transaction"
 
 automatedTxnParser :: TokenParsing m => m RawEntity
-automatedTxnParser = undefined
+automatedTxnParser = RawAutoTxnEntity <$> go
+  where go = RawAutoTxn
+             <$> (tokenP (char '=') *>
+                  (pack <$> manyTill anyChar (try (lookAhead endOfLine))))
+             <*> (endOfLine *> some postingParser)
+             <?> "automated transaction"
 
 periodicTxnParser :: TokenParsing m => m RawEntity
-periodicTxnParser = undefined
+periodicTxnParser = RawPeriodTxnEntity <$> go
+  where go = RawPeriodTxn
+             <$> (tokenP (char '~') *>
+                  (pack <$> manyTill anyChar (try (lookAhead endOfLine))))
+             <*> (endOfLine *> some postingParser)
+             <?> "periodic transaction"
 
 transactionParser :: TokenParsing m => m RawEntity
 transactionParser = regularTxnParser
@@ -150,10 +180,11 @@ transactionParser = regularTxnParser
 directiveParser :: TokenParsing m => m RawEntity
 directiveParser =
   Directive <$> optional (oneOf "@!")
-            <*> (pack <$> ((:) <$> letter <*> many alphaNum))
-            <*> (skipMany spaceChars *>
-                 optional
-                 (pack <$> manyTill anyChar (try (lookAhead endOfLine)))
+            <*> (pack <$> ((:) <$> letter <*> tokenP (many alphaNum)))
+            <*> (optional
+                 (pack <$>
+                  ((:) <$> noneOf "\r\n"
+                       <*> manyTill anyChar (try (lookAhead endOfLine))))
                  <* endOfLine)
             <?> "directive"
 
@@ -176,21 +207,28 @@ commentParser = FileComment
 whitespaceParser :: TokenParsing m => m RawEntity
 whitespaceParser = Whitespace . pack <$> some space <?> "whitespace"
 
-entityParser :: DeltaParsing m => m (RawEntity,Delta)
-entityParser = (,) <$> rawEntityParser <*> position
-  where rawEntityParser = directiveParser
-                          <|> commentParser
-                          <|> whitespaceParser
-                          <|> transactionParser
-                          <?> "journal"
+entityParser :: TokenParsing m => m RawEntity
+entityParser = directiveParser
+               <|> commentParser
+               <|> whitespaceParser
+               <|> transactionParser
+               <?> "journal"
+
+journalParser :: DeltaParsing m => m [RawEntityInSitu]
+journalParser =
+  many (RawEntityInSitu <$> pure 0 <*> rend <*> entityParser <*> rend)
 
-parseJournalFile :: FilePath -> ByteString -> [RawEntity]
+parseJournalFile :: FilePath -> ByteString -> Result [RawEntityInSitu]
 parseJournalFile file contents =
   let filepath = either id id $ toText file
-  in go contents (Directed (encodeUtf8 filepath) 0 0 0 0)
-  where go bs delt =
-          case parseByteString entityParser delt bs of
-            Success (result',delt') -> result' : go bs delt'
-            Failure err -> error (show err)
+      start    = Directed (encodeUtf8 filepath) 0 0 0 0
+  in zipWith (\e i -> e { rawEntityIndex = i})
+       <$> parseByteString journalParser start contents
+       <*> pure [1..]
+
+testme :: IO (Result [RawEntityInSitu])
+testme =
+  let file = "/Users/johnw/Documents/Finances/ledger.dat"
+  in parseJournalFile (fromText (pack file)) <$> B.readFile file
 
 -- Text.hs ends here
